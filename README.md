16b077aba38f51f617b4a5045e4efb40473b6dd0
Jason Watmore <jason@pointblankdevelopment.com.au>
2018-10-26 08:36:11 +1100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-7-registration-login-example
+
+Angular 7 User Registration and Login Example with Webpack 4
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,31 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { first } from 'rxjs/operators';
+
+import { User } from '../_models';
+import { UserService } from '../_services';
+
+@Component({templateUrl: 'home.component.html'})
+export class HomeComponent implements OnInit {
+    currentUser: User;
+    users: User[] = [];
+
+    constructor(private userService: UserService) {
+        this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    }
+
+    ngOnInit() {
+        this.loadAllUsers();
+    }
+
+    deleteUser(id: number) {
+        this.userService.delete(id).pipe(first()).subscribe(() => { 
+            this.loadAllUsers() 
+        });
+    }
+
+    private loadAllUsers() {
+        this.userService.getAll().pipe(first()).subscribe(users => { 
+            this.users = users; 
+        });
+    }
+}
